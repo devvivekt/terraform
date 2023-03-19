07c2a2065ef0ab79487c36b3aca00ec96258525b
Vivek Tiwari <reachtovivek.t@gmail.com>
2023-03-19 13:20:24 +0530

    using locals

--- deployingresourcewithcount/main.tf
+++ deployingresourcewithcount/main.tf
@@ -14,6 +14,13 @@ variable "instancetagname_list" {
     default = ["myfirstec2instance","mysecondec2instance","mythirdec2instance"]
 }
 
+locals {
+  common_tags = {
+    Owner = "mydev team"
+    service = "backend"
+  }
+} 
+
 variable "istest" {
   default = false
 }
@@ -23,16 +30,12 @@ resource "aws_instance" "app_server_dev" {
   ami           = "ami-02f3f602d23f1659d"
   instance_type = "t2.micro"
   count = var.istest == true ? 2 : 0
-  tags = {
-    Name = var.instancetagname_list[count.index]
-  }
+  tags = local.common_tags
 }
 
 resource "aws_instance" "app_server_prod" {
   ami           = "ami-02f3f602d23f1659d"
   instance_type = "t2.micro"
   count = var.istest == false ? 3 : 0
-  tags = {
-    Name = var.instancetagname_list[count.index]
-  }
+  tags = local.common_tags
 }
