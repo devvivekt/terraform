995dbe71b29c795c0846ee1ea52fcd4fa07a4a4c
Vivek Tiwari <reachtovivek.t@gmail.com>
2023-03-19 13:13:22 +0530

    conditionalvariable

--- deployingresourcewithcount/main.tf
+++ deployingresourcewithcount/main.tf
@@ -14,11 +14,24 @@ variable "instancetagname_list" {
     default = ["myfirstec2instance","mysecondec2instance","mythirdec2instance"]
 }
 
+variable "istest" {
+  default = false
+}
 
-resource "aws_instance" "app_server" {
+
+resource "aws_instance" "app_server_dev" {
   ami           = "ami-02f3f602d23f1659d"
   instance_type = "t2.micro"
-  count = 3
+  count = var.istest == true ? 2 : 0
+  tags = {
+    Name = var.instancetagname_list[count.index]
+  }
+}
+
+resource "aws_instance" "app_server_prod" {
+  ami           = "ami-02f3f602d23f1659d"
+  instance_type = "t2.micro"
+  count = var.istest == false ? 3 : 0
   tags = {
     Name = var.instancetagname_list[count.index]
   }
